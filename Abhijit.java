23130105594fbde76a46ba361256cad4101c2157
AbhijitGaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2025-12-10 19:33:06 +0530

    checking webhook

--- Abhijit.java
+++ Abhijit.java
@@ -65,5 +65,31 @@ public class Abhijit {
         }
         return true;
     }
+
+
+
+    public static void is_prime2(String[] args) {
+        int n = 20; // You can change this value to test with different numbers
+
+        System.out.println("Prime numbers from 1 to " + n + ":");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime2(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
