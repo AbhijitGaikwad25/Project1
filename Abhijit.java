fa5c8e89931f4d711e0347455d71940ff3f07f6b
AbhijitGaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2025-12-11 20:19:44 +0530

    update remoteChanges

--- Abhijit.java
+++ Abhijit.java
@@ -156,11 +156,6 @@ public class Abhijit {
         if (num <= 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
         return true;
     }
 }
