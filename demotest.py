ee1f2d110d2d6ac40ea1166fc30f20ad038e8800
AbhijitGaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2025-12-18 15:08:11 +0530

    adding py file

---
+++ demotest.py
@@ -0,0 +1,16 @@
+# This file is intentionally created to test metadata extraction
+
+from utils.jwt import JWTUtil
+from models.user import UserModel
+from fastapi import Depends
+
+class AuthService(UserModel):
+    def __init__(self):
+        pass
+
+    def login(self, username: str, password: str):
+        token = JWTUtil.generate_token({"user": username})
+        return {"token": token}
+
+def helper_function():
+    print("This is a helper function")
